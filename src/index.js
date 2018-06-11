daff22c6ab9cad6081ee50c40514243b1ee6b27a
Kalvin Lam <klam@babbel.com>
2018-06-11 14:44:54 -0400

    Save changes

--- src/index.js
+++ src/index.js
@@ -80,15 +80,17 @@ class Board extends React.Component {
 class Game extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
+      <Router>
+        <div className="game">
+          <div className="game-board">
+            <Board />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
         </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
+      </Router>
     );
   }
 }
